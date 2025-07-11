5baa4144891441ec442ce42232876c9d40f9fba2
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-07-11 14:00:01 -0300

    Update 05_JOINs.sql

--- scripts/05_JOINs.sql
+++ scripts/05_JOINs.sql
@@ -90,4 +90,36 @@ LEFT JOIN orders AS o
 ON c.id = o.customer_id
 WHERE o.customer_id IS NOT NULL
 
--- 
+-- Generate all possible combinations of customers and orders
+SELECT *
+FROM customers
+CROSS JOIN orders;
+
+-- Using SalesDB, retrieve a list of all orders, along with the related customer, product, and employee details
+/*
+       For each order, display:
+              - Order ID
+              - Customer's name
+              - Product name
+              - Sales amount
+              - Product price
+              - Salesperson's name
+*/
+USE SalesDB
+
+SELECT
+	o.OrderID,
+	o.Sales,
+	c.FirstName AS CustomerFirstName,
+	c.LastName AS CustomerLastName,
+	p.Product AS ProductName
+	p.price,
+	e.FirstName AS EmployeeFirstName,
+	e.LastName AS EmployeeLastName
+FROM Sales.Orders AS o
+LEFT JOIN Sales.Customers AS c
+ON o.CustomerID = c.CustomerID
+LEFT JOIN Sales.Products AS p
+ON o.ProductID = p.ProductID
+LEFT JOIN Sales.Employee AS e
+ON o.SalesPersonID = e .EmployeeID;

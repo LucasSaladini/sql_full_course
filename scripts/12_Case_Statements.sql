e405979ff173604b3443e896a1871c973dd2f86a
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-03 10:48:45 -0300

    Update 12_Case_Statements.sql

--- scripts/12_Case_Statements.sql
+++ scripts/12_Case_Statements.sql
@@ -78,3 +78,13 @@ SELECT
   AVG(Score) OVER () AS AvgCustomer
 FROM Sales.Customers;
 
+-- Count how many times each customer has made an order with sales greater than 30
+SELECT
+   CustomerID,
+   SUM(CASE
+      WHEN Sales > 30 THEN 1
+      ELSE 0
+   END) TotalOrdersHighSales,
+   COUNT(*) TotalOrders
+FROM Sales.Orders
+GROUP BY CustomerID;

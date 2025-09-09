15c7353f8e1635cf9a8ddb4c3d1f82656aa98d30
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-09 16:36:01 -0300

    Update 18_Subqueries.sql

--- scripts/18_Subqueries.sql
+++ scripts/18_Subqueries.sql
@@ -1,1 +1,26 @@
+-- Find the products that have a price higher than the average price of all products
+--Main query
+SELECT
+  *
+FROM (
+  --Subquery
+  SELECT
+    ProductID,
+    Price,
+    AVG(Price) OVER () AvgPrice
+  FROM Sales.Products)t
+WHERE price > AvgPrice
+
+-- Rank customers based on their total amount of sales
+-- Main query
+SELECT
+  *,
+  RANK() OVER (ORDER BY TotalSales DESC) CustomerRank
+FROM 
+  -- Subquery
+  (SELECT
+    CustomerID,
+    SUM(Sales) TotalSales
+  FROM Sales.Orders
+  GROUP BY CustomerID)t
 

8d7cdc1e27e8c73d62e31ee60312e1583353fd0c
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-09 16:53:41 -0300

    Update 18_Subqueries.sql

--- scripts/18_Subqueries.sql
+++ scripts/18_Subqueries.sql
@@ -24,3 +24,23 @@ FROM
   FROM Sales.Orders
   GROUP BY CustomerID)t
 
+-- Show the products IDs, product names, prices, and the total number of orders (Scalar subquery)
+SELECT
+ ProductID,
+ Product,
+ Price,
+ (SELECT COUNT(*) TotalOrders FROM Sales.Orders) AS TotalOrders
+FROM Sales.Products
+
+-- Show all customer details and find the total orders for each customer
+SELECT
+ c.*,
+ o.TotalOrders
+FROM Sales.Customers c
+LEFT JOIN (
+ SELECT
+  CustomerID
+  COUNT(*) TotalOrders
+ FROM Sales.Orders
+ GROUP BY CustomerID) o
+ON c.CustomerID = o.CustomerID

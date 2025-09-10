f454846889aa4b7b18adc772e163ca358e415c5d
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-10 08:35:51 -0300

    Update 18_Subqueries.sql

--- scripts/18_Subqueries.sql
+++ scripts/18_Subqueries.sql
@@ -81,3 +81,21 @@ WHERE CustomerID NOT IN
                  CustomerID
                 FROM Sales.Customers
                 WHERE Country = 'Germany')
+
+-- Find female employees whose salaries are greater thant the salaries of any male employees
+SELECT
+ EmployeeID,
+ FirstName,
+ Salary
+FROM Sales.Employees
+WHERE Gender = 'F'
+AND Salary > ANY (SELECT Salary FROM Sales.Employees WHERE Gender = 'M')
+
+-- Find female employees whose salaries are greater than the salaries of all male employees
+SELECT
+ EmployeeID,
+ FirstName,
+ Salary
+FROM Sales.Employees
+WHERE Gender = 'F'
+AND Salary > ALL (SELECT Salary FROM Sales.Employees WHERE Gender = 'M')

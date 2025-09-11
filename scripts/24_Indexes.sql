2ef5683e71f4b8fb065bc23c4684fe85976334eb
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-11 15:37:18 -0300

    Update 24_Indexes.sql

--- scripts/24_Indexes.sql
+++ scripts/24_Indexes.sql
@@ -13,3 +13,5 @@ ON Sales.DBCustomers (LastName)
 CREATE INDEX idx_DBCustomers_FirstName
 ON Sales.DBCustomers (FirstName)
 
+CREATE INDEX idx_DBCustomers_CountryScore
+ON Sales.DBCustomers (Country, Score)

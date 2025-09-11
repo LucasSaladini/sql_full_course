eff59b66aec6a983e777c73fe99d6e60df7579bf
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-11 16:04:34 -0300

    Update 24_Indexes.sql

--- scripts/24_Indexes.sql
+++ scripts/24_Indexes.sql
@@ -15,3 +15,36 @@ ON Sales.DBCustomers (FirstName)
 
 CREATE INDEX idx_DBCustomers_CountryScore
 ON Sales.DBCustomers (Country, Score)
+
+DROP INDEX [idx_DBCustomers_CustomerID] ON Sales.DBCustomers
+
+CREATE CLUSTERED COLUMNSTORE INDEX idx_DBCustomers_CS
+ON Sales.DBCustomers
+
+DROP INDEX idx_DBCustomers_CS ON Sales.DBCustomers
+
+CREATE NONCLUSTERED COLUMNSTORE INDEX idx_DBCustomers_CS_FirstName
+ON Sales.DBCustomers
+
+USE AdventureWorksDW2022;
+
+-- HEAP
+SELECT *
+INTO FactInternetSales_HP
+FROM FactInternetSales
+
+-- RowStore
+SELECT *
+INTO FactInternetSales_RS
+FROM FactInternetSales
+
+CREATE CLUSTERED INDEX idx_FactsInternetSales_RS_PK
+ON FactInternetSales_RS (SalesOrderNumber, SalesOrderLineNumber)
+
+-- ColumnStore
+SELECT *
+INTO FactInternetSales_CS
+FROM FactInternetSales
+
+CREATE CLUSTERED COLUMNSTORE INDEX idx_FactsInternetSales_CS_PK
+ON FactInternetSales_CS

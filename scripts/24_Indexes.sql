1bd69a2510fe15065d97ab58c54bf26726101ae2
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-11 16:10:15 -0300

    Update 24_Indexes.sql

--- scripts/24_Indexes.sql
+++ scripts/24_Indexes.sql
@@ -48,3 +48,10 @@ FROM FactInternetSales
 
 CREATE CLUSTERED COLUMNSTORE INDEX idx_FactsInternetSales_CS_PK
 ON FactInternetSales_CS
+
+USE SalesDB;
+
+CREATE UNIQUE NONCLUSTERED INDEX idx_Products_Product
+ON Sales.Products (Product)
+
+INSERT INTO Sales.Products (ProductID, Product) VALUES (106, 'Caps')

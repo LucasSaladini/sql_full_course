43bbe9af4bc29a21385c7becb354c4bc5f9a910b
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-11 16:37:55 -0300

    Update 24_Indexes.sql

--- scripts/24_Indexes.sql
+++ scripts/24_Indexes.sql
@@ -59,3 +59,29 @@ INSERT INTO Sales.Products (ProductID, Product) VALUES (106, 'Caps')
 CREATE NONCLUSTERED INDEX idx_Customers_Country
 ON Sales.Customers (Country)
 WHERE Country = 'USA'
+
+-- List all indexes on a specific table
+sp_helpindex 'Sales.DBCustomers'
+
+-- Monitoring Index Usage
+SELECT 
+  tbl.name AS TableName,
+  idx.name AS IndexName,
+  idx.type_desc AS IndexType,
+  idx.is_primary_key AS IsPrimaryKey,
+  idx.is_unique AS IsUnique,
+  idx.is_disabled AS IsDisabled
+  s.user_seeks AS UserSeeks,
+  s.user_scans AS UserScans,
+  s.user_lookups AS UserLookups,
+  s.user_updates AS UserUpdates,
+  COALESCE(s.last_user_seek, s.last_user_scan) LastUpdate
+FROM sys.indexes idx
+JOIN sus.tables tbl
+  ON idx.object_id = tbl.object_id
+LEFT JOIN sys.dm_db_index>usage_stats s
+  ON s.object_id = idx.object_id
+  AND s.index_id = idx.idex_id
+ORDER BY tbl.name, idx.name
+
+SELECT * FROM sys.dm_db_index_usage_stats

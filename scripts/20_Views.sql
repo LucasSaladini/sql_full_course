952d31a448d8df2e6385b5a8a431685e9a84d88d
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-11 08:33:09 -0300

    Update 20_Views.sql

--- scripts/20_Views.sql
+++ scripts/20_Views.sql
@@ -48,3 +48,27 @@ IF OBJECT_ID('V_Monthly_Summary', 'V') IS NOT NULL
 GO
 
 -- Provide a view that combines details from orders, products, customers, and employees
+CREATE VIEW Sales.V_Order_Details AS (
+  SELECT
+    o.OrderID,
+    o.OrderDate,
+    p.Product,
+    p.Category,
+    COALESCE(c.FirstName, '') + ' ' + COALESCE(c.LastName, '') CustomerName,
+    c.Country CustomerCountry,
+    COALESCE(e.FirstName, '') + ' ' + COALESCE(e.LastName, '') SalesName,
+    e.Department,
+    o.Sales,
+    o.Quantity
+  FROM Sales.Orders o
+  LEFT JOIN Sales.Products p
+    ON p.ProductID = o.ProductID
+  LEFT JOIN Sales.Customers c
+    ON c.CustomerID = o.CustomerID
+  LEFT JOIN Sales.Employees e
+    ON e.EmployeeID = o.SalesPersonID
+)
+
+SELECT
+  *
+FROM Sales.V_Order_Details

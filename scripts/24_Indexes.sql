6a766821816055ebc9e17905f1ad08c23a11df1f
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-11 15:28:34 -0300

    Create 24_Indexes.sql

---
+++ scripts/24_Indexes.sql
@@ -0,0 +1,15 @@
+SELECT *
+INTO Sales.DBCustomers
+FROM Sales.Customers
+
+CREATE CLUSTERED INDEX idx_DBCustomers_CustomerID
+ON Sales.DBCustomers (CustomerID)
+
+DROP INDEX idx_DBCustomers_CustomerID ON Sales.DBCustomers
+
+CREATE NONCLUSTERED INDEX idx_DBCustomers_LastName
+ON Sales.DBCustomers (LastName)
+
+CREATE INDEX idx_DBCustomers_FirstName
+ON Sales.DBCustomers (FirstName)
+

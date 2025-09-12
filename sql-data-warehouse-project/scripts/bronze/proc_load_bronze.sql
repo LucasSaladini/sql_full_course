d2f924e64a46c01d41ae490aadd4ef9072f8d73f
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-12 16:50:05 -0300

    Create proc_load_bronze.sql

---
+++ sql-data-warehouse-project/scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,90 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
+TRUNCATE TABLE bronze.crm_cust_info;
+
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\sql\dwh_project\datasets\source_crm\cust_info.csv'
+WITH (
+  FIRSTROW = 2,
+  FIELDTERMINATOR = ',',
+  TABLOCK 
+);
+
+SELECT COUNT(*) FROM bronze.crm_sales_details;
+
+TRUNCATE TABLE bronze.crm_prd_info;
+
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\sql\dwh_project\datasets\source_crm\crm_prd_info.csv'
+WITH (
+  FIRSTROW = 2,
+  FIELDTERMINATOR = ',',
+  TABLOCK 
+);
+
+SELECT COUNT(*) FROM bronze.crm_prd_info;
+
+TRUNCATE TABLE bronze.crm_sales_details;
+
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\sql\dwh_project\datasets\source_crm\crm_sales_details.csv'
+WITH (
+  FIRSTROW = 2,
+  FIELDTERMINATOR = ',',
+  TABLOCK 
+);
+
+SELECT COUNT(*) FROM bronze.crm_prd_info;
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\sql\dwh_project\datasets\source_erp\erp_loc_a101.csv'
+WITH (
+  FIRSTROW = 2,
+  FIELDTERMINATOR = ',',
+  TABLOCK 
+);
+
+SELECT COUNT(*) FROM bronze.erp_loc_a101;
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\sql\dwh_project\datasets\source_erp\erp_cust_az12.csv'
+WITH (
+  FIRSTROW = 2,
+  FIELDTERMINATOR = ',',
+  TABLOCK 
+);
+
+SELECT COUNT(*) FROM bronze.erp_cust_az12;
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\sql\dwh_project\datasets\source_erp\erp_px_cat_g1v2.csv'
+WITH (
+  FIRSTROW = 2,
+  FIELDTERMINATOR = ',',
+  TABLOCK 
+);
+
+SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2;

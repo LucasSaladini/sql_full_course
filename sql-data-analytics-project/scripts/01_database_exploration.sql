484ff4172087059e5b45fa60fb156218c4daaa43
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-15 16:13:54 -0300

    Create 01_database_exploration.sql

---
+++ sql-data-analytics-project/scripts/01_database_exploration.sql
@@ -0,0 +1,16 @@
+-- Explore all objects in the database
+SELECT 
+    TABLE_CATALOG, 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- Explore all columns in the database
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';

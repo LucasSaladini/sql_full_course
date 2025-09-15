d70ed032abdc03b5d9fa7206367f875c6cab5263
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-15 15:25:31 -0300

    Update check_tables.sql

--- sql-data-warehouse-project/scripts/gold/check_tables.sql
+++ sql-data-warehouse-project/scripts/gold/check_tables.sql
@@ -30,6 +30,42 @@ LEFT JOIN silver.erp_cust_az12 ca
   ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
   ON ci.cst_key = la.cid;
-  
+
+SELECT
+  pn.prd_id,
+  pn.cat_id,
+  pn.prd_key,
+  pn.prd_nm,
+  pn.prd_cost,
+  pn.prd_line,
+  pn.prd_start_dt,
+  pn.prd_end_dt,
+  pc.cat,
+  pc.subcat,
+  pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL; -- filter out all historical data
+
+SELECT prd_key, COUNT(*) FROM (
+SELECT
+  pn.prd_id,
+  pn.cat_id,
+  pn.prd_key,
+  pn.prd_nm,
+  pn.prd_cost,
+  pn.prd_line,
+  pn.prd_start_dt,
+  pn.prd_end_dt,
+  pc.cat,
+  pc.subcat,
+  pc.maintenance
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL -- filter out all historical data
+)t GROUP BY prd_key
+HAVING COUNT(*) > 1;
 -- ===============
 

cea8b2084002c10dcbd7e31463a6056db23d97e6
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-15 13:45:58 -0300

    Update proc_load_silver.sql

--- sql-data-warehouse-project/scripts/silver/proc_load_silver.sql
+++ sql-data-warehouse-project/scripts/silver/proc_load_silver.sql
@@ -476,3 +476,49 @@ SELECT
 FROM bronze.crm_sales_details;
 
 SELECT * FROM silver.crm_sales_details;
+
+-- ========== erp.cust_az12 ===================
+-- Identify out-of-range dates
+SELECT DISTINCT
+  dbate
+FROM bronze.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE();
+
+SELECT DISTINCT
+  dbate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE();
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+  gen,
+  CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+       WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+       ELSE 'n/a'
+  END AS gen       
+FROM bronze.erp_cust_az12;
+
+SELECT DISTINCT 
+  gen,
+  CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+       WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+       ELSE 'n/a'
+  END AS gen       
+FROM silver.erp_cust_az12;
+
+-- ======================
+INSERT INTO silver.erp_cust_az12 (cid, bdate, gen)
+SELECT
+  CASE WHEN cid LIKE 'NAS%' THEN SUBSSTRING(cid, 4, LEN(cid))
+    ELSE cid
+  END cid,
+  CASE WHEN bdate > GETDATE() THEN NULL
+       ELSE bdate
+  END AS bdate,
+  CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+       WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+       ELSE 'n/a'
+  END AS gen
+FROM bronze.erp_cust_az12;
+
+SELECT * FROM silver.erp_cust_az12;

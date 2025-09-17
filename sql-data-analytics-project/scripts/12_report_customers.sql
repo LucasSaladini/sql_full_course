00812fe15a162e9d5088d142b998c7f08ba9c35c
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2025-09-17 11:23:06 -0300

    Update 12_report_customers.sql

--- sql-data-analytics-project/scripts/12_report_customers.sql
+++ sql-data-analytics-project/scripts/12_report_customers.sql
@@ -20,6 +20,7 @@ Highlights:
 		- average monthly spend
 ===============================================================================
 */
+CREATE VIEW gold.report_customers AS
 WITH base_query AS (
   SELECT
     f.order_number,
@@ -62,10 +63,28 @@ SELECT
   customer_number,
   customer_name,
   age,
+	CASE WHEN age < 20 THEN 'Under 20'
+		 WHEN age BETWEEN 20 AND 29 THEN '20 - 29'
+		 WHEN age BETWEEN 30 AND 39 THEN '30 - 39'
+		 WHEN age BETWEEN 40 AND 49 THEN '40 - 49'
+		 ELSE '50 and Above'
+	END AS age_group,
+	CASE
+		WHEN lifespan >= 12 AND total_sales > 5000 THEN 'VIP'
+		WHEN lifespan >= 12 AND total_sales <= 5000 THEN 'Regular'
+		ELSE 'New'
+	END AS customer_segment,
   total_orders,
+  DATEDIFF(month, last_order_date, GETDATE()) AS recency,
   total_sales,
   total_quantity,
   total_products,
   last_order_date,
-  lifespan
+  lifespan.
+	CASE WHEN total_orders = 0 THEN 0
+		ELSE total_sales / total_orders AS avg_order_value
+	END AS avg_order_value,
+	CASE WHEN lifespan = 0 THEN total_sales
+		ELSE total_sales / lifespan
+	END AS avg_monthly_spend
 FROM customer_aggregation
